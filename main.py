8536b2a46f08eb6033c00ccf1be63b8354409701
Zaida-3dO <opsijay@gmail.com>
2020-02-20 19:44:21 +0100

    Done with main

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-# libs
 # books
 # noScanDays
 
-def selectLibraries():
-    pass
+
+def selectLibraries(libs):
+    selectedLibraries = []
+    currentStep = 0
+    while currentStep < noScanDays:
+        if len(libs) == 0:
+            break
+        maxLibScore = 0
+        maxLib = None
+        for lib in libs:
+            score = lib.score()
+            if score >= maxLibScore:
+                maxLibScore = score
+                maxLib = lib
+        if maxLib:
+            currentStep = maxLib.select()
+            libs.remove(maxLib)
+            selectedLibraries.append(maxLib)
+        else:
+            break
+    return selectedLibraries
