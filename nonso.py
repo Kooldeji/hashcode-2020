8c7dc8458e0c62c264a9c2f1efccffa3e45f731a
leonardchinonso <leokingluthers@gmail.com>
2020-02-20 22:05:29 +0100

    pick and unpick

--- nonso.py
+++ nonso.py
@@ -4,6 +4,7 @@ class Library:
         self.books = libBooks
         self.signupTime = signupTime
         self.booksPerDay = booksPerDay
+        self.selected = set()
 
     def libraryScore(self, step, days):
         score = 0
@@ -21,6 +22,28 @@ class Library:
             j += 1
         self.books = self.books[:i+1]
         return score
+
+    def pick(self, daysLeft, selectedBooks):
+        totalScore = 0
+        if self.signupTime >= daysLeft: return -1
+        days = daysLeft - self.signupTime
+        scannable = days * self.booksPerDay
+
+        i = j = 0
+        while i <= scannable and j != len(self.books):
+            if self.books[j] not in selectedBooks:
+                totalScore += books[self.books[j]]
+                selectedBooks.add(self.books[j])
+                self.selected.add(self.books[j])
+                i += 1
+            j += 1
+        return totalScore
+
+    def unpick(self, selectedBooks):
+        for book in self.selected:
+            selectedBooks.remove(book)
+        self.selected = set()
+
 
     def select(self, step):
         global scanned
